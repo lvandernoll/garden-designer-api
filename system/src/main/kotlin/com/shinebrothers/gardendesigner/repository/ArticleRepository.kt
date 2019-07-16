d8f99fb4a90733e9d5c1b193fd822db77d677da8
lvandernoll <leonvdnoll@gmail.com>
2019-07-16 11:00:02 +0200

    removed useless stuff

--- system/src/main/kotlin/com/shinebrothers/gardendesigner/repository/ArticleRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.shinebrothers.gardendesigner.repository
-
-import com.shinebrothers.gardendesigner.model.Article
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ArticleRepository : JpaRepository<Article, Long>
